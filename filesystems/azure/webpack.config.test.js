5347f4c66b4f01a735d1e223557588437a7571a9
Yoshihiro Seki <nikochan2k@gmail.com>
2022-09-19 15:12:04 +0900

    modify testcase of web

--- filesystems/azure/webpack.config.test.js
+++ filesystems/azure/webpack.config.test.js
@@ -1,9 +1,6 @@
 module.exports = {
   mode: "development",
   entry: {
-    "basic.spec": "./lib/__tests__/basic.spec.js",
-    "head.spec": "./lib/__tests__/head.spec.js",
-    "list.spec": "./lib/__tests__/list.spec.js",
     "basic-nodir.spec": "./lib/__tests__/basic-nodir.spec.js",
     "head-nodir.spec": "./lib/__tests__/head-nodir.spec.js",
     "list-nodir.spec": "./lib/__tests__/list-nodir.spec.js",
