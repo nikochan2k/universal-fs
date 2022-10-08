16eddbf35ec0907deb145300614470ddf6434102
Yoshihiro Seki <nikochan2k@gmail.com>
2022-10-08 13:14:18 +0900

    refactoring

--- converter/src/converters/DefaultTextHelper.ts
+++ converter/src/converters/DefaultTextHelper.ts
@@ -1,21 +1,14 @@
+import {
+  convert,
+  ConvertArrayBufferOptions,
+  ConvertStringOptions,
+} from "encoding-japanese";
+import "fast-text-encoding";
 import { Charset } from "./core";
 import { TextHelper } from "./TextHelper";
 
-if (!TextDecoder || !TextEncoder) {
-  require("fast-text-encoding");
-}
-
 const textEncoder = new TextEncoder();
 const textDecoder = new TextDecoder();
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-let convert: any;
-try {
-  // eslint-disable-next-line
-  convert = require("encoding-japanese").convert;
-} catch {
-  // Do nothing
-}
 
 function textToUtf16leBuffer(text: string) {
   const ab = new ArrayBuffer(text.length * 2);
@@ -26,34 +19,58 @@ function textToUtf16leBuffer(text: string) {
   return ab;
 }
 export class DefaultTextHelper implements TextHelper {
-  bufferToText(u8: Uint8Array, bufCharset: Charset): Promise<string> {
+  _convert: typeof convert | undefined | null;
+
+  private async convert(
+    data: Uint8Array,
+    options: ConvertStringOptions
+  ): Promise<string>;
+  private async convert(
+    data: string,
+    options: ConvertArrayBufferOptions
+  ): Promise<ArrayBuffer>;
+  private async convert(
+    data: Uint8Array | string,
+    options: ConvertStringOptions | ConvertArrayBufferOptions
+  ): Promise<ArrayBuffer | string> {
+    if (typeof this._convert === "undefined") {
+      try {
+        this._convert = (await import("encoding-japanese")).convert;
+      } catch {
+        this._convert = null;
+      }
+    }
+
+    if (!this._convert) {
+      throw new Error(
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        `Illegal encoding: from=${options.from}, to=${options.to}`
+      );
+    }
+
+    if (typeof data === "string") {
+      return convert(data, options as ConvertArrayBufferOptions);
+    } else {
+      return convert(data, options as ConvertStringOptions);
+    }
+  }
+
+  async bufferToText(u8: Uint8Array, bufCharset: Charset): Promise<string> {
     if (bufCharset === "utf8") {
       return Promise.resolve(textDecoder.decode(u8));
     }
     if (bufCharset === "utf16le") {
       return Promise.resolve(String.fromCharCode.apply(null, Array.from(u8)));
     }
-    if (convert) {
-      try {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        return Promise.resolve(
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-          convert(u8, {
-            to: "UNICODE",
-            from: bufCharset.toUpperCase(),
-            type: "string",
-          })
-        );
-      } catch {
-        // Do nothing
-      }
-    }
 
-    // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-    throw new Error("Illegal encoding: " + bufCharset);
+    return this.convert(u8, {
+      to: "UNICODE",
+      from: bufCharset.toUpperCase(),
+      type: "string",
+    } as ConvertStringOptions);
   }
 
-  textToBuffer(text: string, bufCharset: Charset): Promise<Uint8Array> {
+  async textToBuffer(text: string, bufCharset: Charset): Promise<Uint8Array> {
     if (bufCharset === "utf8") {
       return Promise.resolve(textEncoder.encode(text));
     }
@@ -61,22 +78,13 @@ export class DefaultTextHelper implements TextHelper {
       const ab = textToUtf16leBuffer(text);
       return Promise.resolve(new Uint8Array(ab));
     }
-    if (convert) {
-      try {
-        // eslint-disable-next-line
-        const ab: ArrayBuffer = convert(text, {
-          to: bufCharset.toUpperCase(),
-          from: "UNICODE",
-          type: "arraybuffer",
-        });
-        return Promise.resolve(new Uint8Array(ab));
-      } catch {
-        // Do nothing
-      }
-    }
 
-    // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
-    throw new Error("Illegal encoding: " + bufCharset);
+    const ab = await this.convert(text, {
+      to: bufCharset.toUpperCase(),
+      from: "UNICODE",
+      type: "arraybuffer",
+    } as ConvertArrayBufferOptions);
+    return Promise.resolve(new Uint8Array(ab));
   }
 }
 
