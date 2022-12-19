ace9e4a4ded07d0dc6b9103f6452ba04d280b024
Yoshihiro Seki <nikochan2k@gmail.com>
2022-12-19 23:16:44 +0900

    add BinaryHandler

---
+++ converters/src/handlers/binary.ts
@@ -0,0 +1,41 @@
+import { AbstractHandler, SliceOptions } from "../core";
+
+export const EMPTY_BINARY = "";
+
+class BinaryHandler extends AbstractHandler<string> {
+  public empty(): Promise<string> {
+    return Promise.resolve(EMPTY_BINARY);
+  }
+
+  protected _isEmpty(src: string): Promise<boolean> {
+    return Promise.resolve(src.length === 0);
+  }
+
+  protected async _merge(src: string[]): Promise<string> {
+    return Promise.resolve(src.join(""));
+  }
+
+  protected _size(src: string): Promise<number> {
+    return Promise.resolve(src.length);
+  }
+
+  protected _slice(src: string, options?: SliceOptions): Promise<string> {
+    const start = options?.start ?? 0;
+    let end: number | undefined;
+    if (options?.length != null) {
+      end = start + options.length;
+      if (src.length < end) {
+        end = src.length;
+      }
+    }
+    return Promise.resolve(src.slice(start, length));
+  }
+
+  protected _validateSource(src: string): void {
+    if (typeof src !== "string") {
+      throw new TypeError("src is not string");
+    }
+  }
+}
+
+export default new BinaryHandler();
