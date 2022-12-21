b5ba65c16093e070dfdfd88800a402c11201cbd7
Yoshihiro Seki <nikochan2k@gmail.com>
2022-12-21 22:27:30 +0900

    add base64 testcase

---
+++ converters/src/__tests__/converters/base64/binary.spec.ts
@@ -0,0 +1,7 @@
+import b2b from "../../../converters/base64/binary";
+
+it("convert", async () => {
+  const src = "YWJj";
+  const actual = await b2b.convert(src, { srcType: "base64" });
+  expect(actual).toBe("abc");
+});
