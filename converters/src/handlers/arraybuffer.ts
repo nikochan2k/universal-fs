be90147bfebb9263b804a2f826c908fdef9c137a
Yoshihiro Seki <nikochan2k@gmail.com>
2023-01-02 11:23:16 +0900

    refactoring

--- converters/src/handlers/arraybuffer.ts
+++ converters/src/handlers/arraybuffer.ts
@@ -1,54 +1,20 @@
-import { AbstractHandler, SliceOptions } from "../core";
-import { newBuffer } from "../util";
+import { ArrayBufferLikeHandler } from "./arraybufferlike";
 
 export const EMPTY_ARRAY_BUFFER = new ArrayBuffer(0);
 
-class ArrayBufferHandler extends AbstractHandler<ArrayBufferLike> {
+class ArrayBufferHandler extends ArrayBufferLikeHandler<ArrayBuffer> {
   public name = ArrayBuffer.name;
 
-  public empty(): Promise<ArrayBufferLike> {
+  public empty(): Promise<ArrayBuffer> {
     return Promise.resolve(EMPTY_ARRAY_BUFFER);
   }
 
-  protected _isEmpty(src: ArrayBufferLike): Promise<boolean> {
-    return Promise.resolve(src.byteLength === 0);
+  protected _validateSource(src: ArrayBuffer): boolean {
+    return src instanceof ArrayBuffer;
   }
 
-  protected _merge(src: ArrayBufferLike[]): Promise<ArrayBufferLike> {
-    const byteLength = src.reduce((sum, chunk) => {
-      return sum + chunk.byteLength;
-    }, 0);
-    const u8 = newBuffer(byteLength);
-    let pos = 0;
-    for (const chunk of src) {
-      u8.set(newBuffer(chunk), pos);
-      pos += chunk.byteLength;
-    }
-    return Promise.resolve(u8.buffer);
-  }
-
-  protected _size(src: ArrayBufferLike): Promise<number> {
-    return Promise.resolve(src.byteLength);
-  }
-
-  protected _slice(
-    src: ArrayBufferLike,
-    options?: SliceOptions
-  ): Promise<ArrayBufferLike> {
-    const start = options?.start ?? 0;
-    let end: number | undefined;
-    if (options?.length != null) {
-      end = start + options.length;
-      if (src.byteLength < end) {
-        end = src.byteLength;
-      }
-    }
-    const sliced = src.slice(start, end);
-    return Promise.resolve(sliced);
-  }
-
-  protected _validateSource(src: ArrayBufferLike): boolean {
-    return src instanceof ArrayBuffer || src instanceof SharedArrayBuffer;
+  protected newArrayBufferLike(byteLength: number): ArrayBuffer {
+    return new ArrayBuffer(byteLength);
   }
 }
 
---
+++ converters/src/handlers/arraybufferlike.ts
@@ -0,0 +1,43 @@
+import { AbstractHandler, SliceOptions } from "../core";
+import { newBuffer } from "../util";
+
+export abstract class ArrayBufferLikeHandler<
+  T extends ArrayBufferLike
+> extends AbstractHandler<ArrayBufferLike> {
+  protected _isEmpty(src: T): Promise<boolean> {
+    return Promise.resolve(src.byteLength === 0);
+  }
+
+  protected _merge(src: T[]): Promise<T> {
+    const byteLength = src.reduce((sum, chunk) => {
+      return sum + chunk.byteLength;
+    }, 0);
+    const abl = this.newArrayBufferLike(byteLength);
+    const u8 = newBuffer(abl);
+    let pos = 0;
+    for (const chunk of src) {
+      u8.set(newBuffer(chunk), pos);
+      pos += chunk.byteLength;
+    }
+    return Promise.resolve(u8.buffer as T);
+  }
+
+  protected _size(src: T): Promise<number> {
+    return Promise.resolve(src.byteLength);
+  }
+
+  protected _slice(src: T, options?: SliceOptions): Promise<T> {
+    const start = options?.start ?? 0;
+    let end: number | undefined;
+    if (options?.length != null) {
+      end = start + options.length;
+      if (src.byteLength < end) {
+        end = src.byteLength;
+      }
+    }
+    const sliced = src.slice(start, end);
+    return Promise.resolve(sliced as T);
+  }
+
+  protected abstract newArrayBufferLike(byteLength: number): T;
+}
