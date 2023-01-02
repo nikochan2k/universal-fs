383903236796146df4422bcaa1c2e6fef1f3a676
Yoshihiro Seki <nikochan2k@gmail.com>
2023-01-02 14:59:56 +0900

    add testcases of Reader

--- converters/src/__tests__/converters/reader/arraybuffer.debug-node-spec.ts
+++ converters/src/__tests__/converters/reader/arraybuffer.debug-node-spec.ts
@@ -1,12 +1,13 @@
-import r2u from "../../../converters/reader/uint8array";
+import r2a from "../../../converters/reader/arraybuffer";
 import { createReadable } from "../../supports/Reader";
 
 it("convert", async () => {
   const data = [97, 98, 99];
   const src = createReadable([97, 98, 99]);
-  const actual = await r2u.convert(src);
+  const actual = await r2a.convert(src);
+  const u8 = new Uint8Array(actual);
   expect(actual.byteLength).toEqual(3);
   for (let i = 0; i < data.length; i++) {
-    expect(actual.at(i)).toBe(data[i]);
+    expect(u8.at(i)).toBe(data[i]);
   }
 });
---
+++ converters/src/converters/reader/arraybuffer.ts
@@ -0,0 +1,17 @@
+import type { Readable } from "stream";
+import { handleReadable } from "../../supports/NodeStream";
+import { AbstractConverter } from "../../UnivConv";
+
+class Reader_ArrayBuffer extends AbstractConverter<Readable, ArrayBuffer> {
+  public async _convert(src: Readable): Promise<ArrayBuffer> {
+    const chunks: Buffer[] = [];
+    await handleReadable(src, (chunk) => {
+      chunks.push(chunk);
+      return Promise.resolve();
+    });
+    const buf = Buffer.concat(chunks);
+    return buf.buffer.slice(buf.byteOffset, buf.byteOffset + buf.byteLength);
+  }
+}
+
+export default new Reader_ArrayBuffer();
