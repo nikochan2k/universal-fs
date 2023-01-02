14382bdb05469a365ef9481dc6c15b65d2c2bd34
Yoshihiro Seki <nikochan2k@gmail.com>
2023-01-02 20:32:34 +0900

    fix testcase of blob

--- converters/src/__tests__/handlers/blob.web-spec.ts
+++ converters/src/__tests__/handlers/blob.web-spec.ts
@@ -3,7 +3,8 @@ import bh from "../../handlers/blob";
 it("empty", async () => {
   const actual = await bh.empty();
   expect(actual instanceof Blob).toBe(true);
-  expect(actual.length).toBe(0);
+  console.warn(actual);
+  expect(actual.size).toBe(0);
 });
 
 it("isEmpty", async () => {
@@ -22,13 +23,13 @@ it("merge", async () => {
   const chunk2 = new Blob(["bc"]);
   let actual: Blob;
   actual = await bh.merge([]);
-  expect(actual.length).toBe(0);
+  expect(actual.size).toBe(0);
   actual = await bh.merge([chunk0]);
-  expect(actual.length).toBe(0);
+  expect(actual.size).toBe(0);
   actual = await bh.merge([chunk1]);
-  expect(actual.length).toBe(1);
+  expect(actual.size).toBe(1);
   actual = await bh.merge([chunk1, chunk2]);
-  expect(actual.length).toBe(3);
+  expect(actual.size).toBe(3);
 });
 
 it("size", async () => {
@@ -45,15 +46,15 @@ it("slice", async () => {
   const notEmpty = new Blob(["abc"]);
   let actual: Blob;
   actual = await bh.slice(notEmpty, { length: 0 });
-  expect(actual.length).toBe(0);
+  expect(actual.size).toBe(0);
   actual = await bh.slice(notEmpty, { length: 1 });
-  expect(actual.length).toBe(1);
+  expect(actual.size).toBe(1);
   actual = await bh.slice(notEmpty, { start: 1 });
-  expect(actual.length).toBe(2);
+  expect(actual.size).toBe(2);
   actual = await bh.slice(notEmpty, { start: 1, length: 1 });
-  expect(actual.length).toBe(1);
+  expect(actual.size).toBe(1);
   actual = await bh.slice(notEmpty, { start: 1, length: 3 });
-  expect(actual.length).toBe(2);
+  expect(actual.size).toBe(2);
   actual = await bh.slice(notEmpty, { start: 3 });
-  expect(actual.length).toBe(0);
+  expect(actual.size).toBe(0);
 });
